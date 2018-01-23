99590ebb1ac4112317e245c65e243d8e9507e324
abush <abush@PUNKOR-PC>
2018-01-23 22:44:22 +0200

    remoting-server

--- RemotingOneServer1/Program.cs
+++ RemotingOneServer1/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using RemotingOne;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,12 @@ namespace RemotingOneServer1
     {
         static void Main(string[] args)
         {
+            var channel = new TcpServerChannel(8086);
+            ChannelServices.RegisterChannel(channel, true);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                  typeof(Hello), "Hi", WellKnownObjectMode.SingleCall);
+            Console.WriteLine("Press return to exit");
+            Console.ReadLine();
         }
     }
 }
